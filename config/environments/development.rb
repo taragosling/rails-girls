ca1a1b652eec6384c4612f1d9f0c78fafc35a153
Tara Gosling <tara.gosling@everydayhero.com.au>
2017-07-22 03:29:35 +0000

    add comments and users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,11 +38,7 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
   config.assets.debug = true
-
-  # Suppress logger output for asset requests.
   config.assets.quiet = true
 
   # Raises error for missing translations
@@ -51,4 +47,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
---
+++ db/migrate/20170722021412_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.string :user_name
+      t.string :string
+      t.text :body
+      t.integer :idea_id
+
+      t.timestamps
+    end
+  end
+end
